e7c4042c7611d109c24b1f0ae4d5084cc0dfdbb3
Coypirus <coypirus@gmail.com>
2022-05-14 15:34:32 -0400

    PreliminaryJoystickSagarika

---
+++ JoystickButton12Sagarika/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,74 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+
+  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+
+  public static Joystick sagarikaJoystick = new Joystick(0);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+    sliderTriggerCheck();
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    new JoystickButton(sagarikaJoystick, 12)
+    .whenPressed(new PrintCommand("Button 12 was pressed!"))
+    .whenReleased(new PrintCommand("Button 12 was released!"));
+    Trigger checkSlider = new Trigger(() -> sliderTriggerCheck());
+    checkSlider.whileActiveContinuous(new PrintCommand("The slider has moved to 1."))
+    .whenInactive(new PrintCommand("The slider is not 1."));
+  
+  }
+
+  private boolean sliderTriggerCheck(){
+
+    if(RobotContainer.sagarikaJoystick.getRawAxis(3) == 1.0)
+    {
+      return true;
+    }
+
+    return false;
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return m_autoCommand;
+  }
+}
