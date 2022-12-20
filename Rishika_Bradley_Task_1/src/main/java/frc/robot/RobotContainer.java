6ec4aa62b4b6f7b617734a673f3b0858ba3bba00
cyong120 <cyong120@gmail.com>
2022-12-19 20:24:53 -0500

    PrintJoystickRelease

--- Rishika_Bradley_Task_1/src/main/java/frc/robot/RobotContainer.java
+++ Rishika_Bradley_Task_1/src/main/java/frc/robot/RobotContainer.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.PrintJoystickCommand;
+import frc.robot.commands.PrintJoystickRelease;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -48,6 +49,7 @@ public class RobotContainer {
    */
   private void configureButtonBindings() {
     printButton.whenPressed(new PrintJoystickCommand());
+    printButton.whenReleased(new PrintJoystickRelease());
   }
 
   /**
